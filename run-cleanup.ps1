ce41eb0bb1f644d7554d4829e9a0b1aaf804c05c
sgkens <sgkens@gmail.com>
2026-01-10 13:41:14 +1100

    chore: adding run-cleanup.ps1 clean up script

--- run-cleanup.ps1
+++ run-cleanup.ps1
@@ -1,2 +1,6 @@
 # Remove all build temp files, .dlls, .nupkg, .nuspec from rootfolder
 # usefull if building locally
+
+$interLogger.invoke("Cleanup", "Starting cleanup of build artifacts", $false, 'info')
+
+# clear up here
